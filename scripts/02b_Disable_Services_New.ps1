bcb575a67e73c7a5707f52867bb5938521d7e82b
FWC-9696 <FWC@FWC.com>
2025-02-03 11:57:54 -0500

    Added many services to the list for Manual Startup. Fixed Solitaire script for more reliability

--- scripts/02b_Disable_Services_New.ps1
+++ scripts/02b_Disable_Services_New.ps1
@@ -3,27 +3,56 @@
 # It should not cause issues, but services can always be re-enabled using services.msc
 
 $services = @(
+    "BITS"                                      # Background Intelligent Transfer Service
     #"diagnosticshub.standardcollector.service" # Microsoft (R) Diagnostics Hub Standard Collector Service
-    #"DiagTrack"                                # Diagnostics Tracking Service
-    "dmwappushservice"                         # WAP Push Message Routing Service (see known issues)
+    "dmwappushservice"                          # WAP Push Message Routing Service (see known issues)
     #"lfsvc"                                    # Geolocation Service
-    "MapsBroker"                               # Downloaded Maps Manager
-    "NetTcpPortSharing"                        # Net.Tcp Port Sharing Service
-    "RemoteAccess"                             # Routing and Remote Access
-    "RemoteRegistry"                           # Remote Registry
-    "SharedAccess"                             # Internet Connection Sharing (ICS)
-    "TrkWks"                                   # Distributed Link Tracking Client
+    "MapsBroker"                                # Downloaded Maps Manager
+    "NetTcpPortSharing"                         # Net.Tcp Port Sharing Service
+    "RemoteAccess"                              # Routing and Remote Access
+    "RemoteRegistry"                            # Remote Registry
+    "SharedAccess"                              # Internet Connection Sharing (ICS)
+    "TrkWks"                                    # Distributed Link Tracking Client
+    "SysMain"                                   # Superfetch. Can use a lot of disk.
     #"WbioSrvc"                                 # Windows Biometric Service (required for Fingerprint reader / facial detection)
-    #"WlanSvc"                                 # WLAN AutoConfig (WILL DISABLE WIFI)
-    "WMPNetworkSvc"                            # Windows Media Player Network Sharing Service
-    #"wscsvc"                                  # Windows Security Center Service
-    #"WSearch"                                 # Windows Search
+    #"WlanSvc"                                  # WLAN AutoConfig (WILL DISABLE WIFI)
+    "WMPNetworkSvc"                             # Windows Media Player Network Sharing Service
+  
+    "WSearch"                                   # Windows Search
     #"XblAuthManager"                           # Xbox Live Auth Manager
     #"XblGameSave"                              # Xbox Live Game Save Service
     #"XboxNetApiSvc"                            # Xbox Live Networking Service
-    "ndu"                                      # Windows Network Data Usage Monitor
-    # Services which cannot be disabled
-    #"WdNisSvc"
+    "ndu"                                       # Windows Network Data Usage Monitor
+    "RasMan"                                    # Remote Access Connection Manager
+
+    #######Edge Update Servcies
+    "MicrosoftEdgeElevationService"
+    "edgeupdate"
+    "edgeupdatem"
+
+    #######Telemetry & Diagnostics
+    "CDPSvc"
+    "CDPUserSvc_7902f"
+    "DiagTrack"
+    "DPS"                                     #Diagnostic Poilcy Service
+    "diagsvc"
+
+    #######Windows Defender & Antivirus
+    "wscsvc"                                  #Windows Security Center Service
+    "WinDefend"                               #Microsoft Defender
+    "MDCoreSvc"                               #Microcoft Defender Core
+    "WdNisSvc"                                #Microsoft Defender Network Inspection
+
+    #######3RD Party Services. May or may not be present.
+    "Apple Mobile Device Service"             #Apple Mobile Device, comes with iTunes
+    "FoxitReaderUpdateService"                #Foxit PDF Updater
+    "LGHUBUpdaterService"                     #Logi G Hub Updater
+    "Parsec"                                  #Parsec
+    "RvControlSvc"                            #Radmin VPN
+    "EasyAntiCheat_EOS"                       #Easy Anti-Cheat Epic Games
+    "EasyAntiCheat"                           #Easy Anti-Cheat
+    "EpicOnlineServices"                      #Epic Games
+
 )
 
 foreach ($service in $services) {
@@ -32,6 +61,8 @@ foreach ($service in $services) {
 }
 
 Set-Service HPPrintScanDoctorService -StartupType Disabled -ErrorAction SilentlyContinue
+
+Set-Service XblAuthManager -StartupType Automatic -ErrorAction SilentlyContinue #Set Xbox Live to start with computer
 
 taskmgr /0 /startup
 
--- startup/Solitaire.ps1
+++ startup/Solitaire.ps1
@@ -1,5 +1,4 @@
 #explorer shell:appsFolder\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe!App                 #Game Bar
+Stop-Process -Name Solitaire -ErrorAction SilentlyContinue                              #Kill any other solitaire process
 sleep 30
 explorer shell:appsFolder\Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe!App	    #Microsoft Solitaire Colletion NOTE: Use Get:StartApps for list of UWP Apps
-sleep 1800                                                                          #wait 15.5 minutes for Microsoft Rewards points
-Stop-Process -Name Solitaire                                                            #Kill Solitaire to save system resources
