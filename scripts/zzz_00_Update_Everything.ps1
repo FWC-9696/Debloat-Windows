b5f0e6f797142228ad1738854ca3c5db542161b6
James Richter <fwc@fwc.com>
2025-06-03 18:16:52 -0400

    Revised so that recall disable is only attempted if enabled. Revised spacing/line breaks

--- scripts/zzz_00_Update_Everything.ps1
+++ scripts/zzz_00_Update_Everything.ps1
@@ -1,36 +1,47 @@
-#Disable Recall
-Dism /Online /Get-Featureinfo /Featurename:Recall
-Dism /Online /Disable-Feature /Featurename:Recall
+#Disable Recall if Enabled. Developed using https://github.com/antonflor/RecallManager/blob/main/RecallManager.ps1
+$Recall = (dism /Online /Get-FeatureInfo /FeatureName:Recall)
+    if ($Recall -like "*State : Enabled*") {
+        Dism /Online /Disable-Feature /Featurename:Recall
+        }
+    if ($Recall -like "*State : Disabled*") {
+        Write-Host 
+        Write-Host "Recall is already disabled."
+        }
 
 #NVCleanstall (Updates Graphics Drivers)
-Write-Host `n"Checking for Nvidia Driver Updates (if NVCleanstall is installed)..."
+Write-Host 
+Write-Host "Checking for Nvidia Driver Updates (if NVCleanstall is installed)..."
 try{Start $env:ProgramFiles\NVCleanstall\NVCleanstall.exe}
 catch{Write-Host "NVCleanstall is not installed."}
 
 #Updates Windows Store Apps
-Write-Host `n"Checking for Windows Store Updates..."
+Write-Host 
+Write-Host "Checking for Windows Store Updates... (Manual -- Must click the button in Microsoft Store!)"
 Start ms-windows-store://downloadsandupdates -ErrorAction SilentlyContinue
 
 #Updates Windows
-Write-Host `n"Checking for Windows Updates..."
+Write-Host 
+Write-Host "Checking for Windows Updates..."
 Start-Process ms-settings:windowsupdate
 USOClient StartInteractiveScan
 
 #Update PowerToys
-Write-Host `n"Checking for PowerToys Updates..."
+Write-Host 
+Write-Host "Checking for PowerToys Updates..."
 try{Start-Process $env:LOCALAPPDATA\PowerToys\PowerToys.Update.exe -Verb RunAs}
 catch{Write-Host "PowerToys is not installed."}
 
 #Updates Other Programs
-Write-Host `n"Checking for Software Updates..."
+Write-Host 
+Write-Host "Checking for Software Updates..."
 winget upgrade
 
-Write-Host `n
-Write-Host `n"To upgrade everything, run the following command:"
+Write-Host 
+Write-Host "To upgrade everything, run the following command:"
 Write-Host "winget upgrade --all --accept-source-agreements --accept-package-agreements"
-Write-Host `n"To upgrade an individual package, run:"
+Write-Host 
+Write-Host "To upgrade an individual package, run:"
 Write-Host "winget upgrade <ID>"
-
+Write-Host
 $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-Write-Host `n 
-Write-Host "Last Run: $date" `n `n
+Write-Host "Last Run: $date" `n
